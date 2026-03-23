0e6f0b5189acc3f7eede1f38836508fe14e0becc
od1000174 <aalrabi@ntgclarity.com>
2026-03-23 16:17:33 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./features/auth/login/login.component";
 import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, NavbarComponent],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, FooterComponent],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {}
